ef225faeb4c2b56930a610ab2e4cd8a03cfdeef6
Amal Roy <118099630+Amal-T-Roy@users.noreply.github.com>
2023-02-02 19:14:49 +0530

    Add files via upload

---
+++ Examples/Even.py
@@ -0,0 +1,12 @@
+x = int(input('Enter the value\n'))
+
+if( x > 0) :
+    if( x % 2) :
+            print('Odd Number\n')
+    else :
+            print('Even Number \n')
+
+elif( x == 0) :
+    print('Null')
+else :
+    print('Negative')
