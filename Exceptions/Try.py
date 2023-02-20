c847487fdba8b5793264788ae718f7009a51c752
amal.roy <itsmeamalroy@gmail.com>
2023-02-20 17:24:53 +0530

    Test

--- Exceptions/Try.py
+++ Exceptions/Try.py
@@ -1,12 +1,18 @@
 
 a = 10
 b = 0
+c = 'k'
 
 # print(a/b)
 
 try : 
     print(a/b)
+    # print(a/c)
+except ValueError as e:
+    print("Value Error")
+except ZeroDivisionError as e:
+    print("Zero Division Error")
 except Exception as e:
-    print("Invalid Operation : ",e)
+    print("Something went wrong: ",e)
 finally:
     print("Operation Completed")
